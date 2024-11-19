746a41862d7eef7429e046f5d1f4c7359fd8f2e3
BencsikG <bgeri91@gmail.com>
2024-11-19 18:27:15 +0100

    Moved existing pictures to misc folder. Changed license blabla so it doesn't get included in doc. Added sphinx stuff in .doc. Initial generation of doc .rsts works.

---
+++ docs/source/gggears.rst
@@ -0,0 +1,69 @@
+gggears package
+===============
+
+Submodules
+----------
+
+gggears.curve module
+--------------------
+
+.. automodule:: gggears.curve
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+gggears.defs module
+-------------------
+
+.. automodule:: gggears.defs
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+gggears.function\_generators module
+-----------------------------------
+
+.. automodule:: gggears.function_generators
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+gggears.gggears\_build123d module
+---------------------------------
+
+.. automodule:: gggears.gggears_build123d
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+gggears.gggears\_convert module
+-------------------------------
+
+.. automodule:: gggears.gggears_convert
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+gggears.gggears\_core module
+----------------------------
+
+.. automodule:: gggears.gggears_core
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+gggears.gggears\_wrapper module
+-------------------------------
+
+.. automodule:: gggears.gggears_wrapper
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: gggears
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- src/gggears/gggears_wrapper.py
+++ src/gggears/gggears_wrapper.py
@@ -1,15 +1,13 @@
-"""
-Copyright 2024 Gergely Bencsik
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+# Copyright 2024 Gergely Bencsik
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import numpy as np
 from gggears.gggears_core import *
@@ -18,6 +16,7 @@ from gggears.gggears_build123d import *
 
 class SpurGear:
     """Class for a basic spur gear.
+
     Parameters
     ----------
     number_of_teeth: int
@@ -350,6 +349,7 @@ class SpurRingGear(SpurGear):
 
 class HelicalGear(SpurGear):
     """Class for a helical gear.
+
     Parameters
     ----------
     number_of_teeth: int
@@ -504,9 +504,8 @@ class HelicalGear(SpurGear):
 
 
 class HelicalRingGear(HelicalGear):
-    """
-    A class representing a helical ring gear, which is a type of gear with teeth on the
-    inner circumference and a helical angle.
+    """A class representing a helical ring gear, which is a type of gear with teeth on
+    the inner circumference and a helical angle.
 
     Parameters
     ----------
