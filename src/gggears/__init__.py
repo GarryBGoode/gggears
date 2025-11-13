7ae7533a77ec9e2296600cb7e980b41b581f8dfb
jdegenstein <jdegenstein@users.noreply.github.com>
2025-11-13 10:06:09 -0600

    __init__.py -> enable setuptools_scm via importlib.metadata

--- src/gggears/__init__.py
+++ src/gggears/__init__.py
@@ -1,3 +1,4 @@
+from importlib.metadata import version, PackageNotFoundError
 from gggears.curve import *
 from gggears.defs import *
 from gggears.function_generators import *
@@ -6,3 +7,9 @@ from gggears.gggears_core import *
 from gggears.gggears_build123d import *
 from gggears.gggears_wrapper import *
 from gggears.gearmath import *
+
+
+try:
+    __version__ = version("bd_warehouse")
+except PackageNotFoundError:
+    __version__ = "unknown version"
